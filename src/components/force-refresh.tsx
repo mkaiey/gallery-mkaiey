1afdf9e9a7b2a2192b087fc37a918e465526e041
novipro88 <noviantipali97@gmail.com>
2024-02-12 16:33:20 +0700

    initial commit

---
+++ src/components/force-refresh.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export function ForceRefresh() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+    // eslint-disable-next-line
+  }, []);
+
+  return <></>;
+}
